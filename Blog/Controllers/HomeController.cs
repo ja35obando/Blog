a98f0ee79007dfae2c35e3fb48dd385e118f62ba
José Andrés Obando Díaz <ja35obando@gmail.com>
2014-03-21 14:59:29 -0600

    Home

--- Blog/Controllers/HomeController.cs
+++ Blog/Controllers/HomeController.cs
@@ -10,7 +10,7 @@ namespace Blog.Controllers
     {
         public ActionResult Index()
         {
-            ViewBag.Message = "Modify this template to jump-start your ASP.NET MVC application.";
+            ViewBag.Message = "Entradas";
 
             return View();
         }
