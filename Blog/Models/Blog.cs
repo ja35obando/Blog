f5c246bcab444027f210e620a62c7a6e1c2bb719
José Andrés Obando Díaz <ja35obando@gmail.com>
2014-04-01 14:29:43 -0600

    Cambios 1.12
    Blog CRUD

---
+++ Blog/Models/Blog.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Blog.Models
+{
+    public class Blog
+    {
+        public int id { get; set; }
+        public string Name { get; set; }
+        public string Detalle { get; set; }
+    }
+
+    public class BlogDbContext : DbContext
+    {
+        public DbSet<Blog> Blogs { get; set; }
+    }
+}
